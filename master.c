701035a88100e55ed4644730bde2593f30a58a69
Roberto Catalan <rcatalan@itba.edu.ar>
2021-03-28 21:09:33 -0300

    Armado del select para escuchar a los esclavos e implementacion slave

--- master.c
+++ master.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/select.h>
 // ​./solve files/* | ./vista
 
 //fd[0] read
@@ -12,51 +13,95 @@
 #define WRITE 1
 #define READ 0
 
-#define FORK_ERROR -1
-#define EXEC_ERROR -2
-#define PIPE_ERROR -3
-#define NARGUMENTS_ERROR -4
-#define DUPPING_ERROR -5
-#define CLOSE_ERROR -6
-
 #define PATH_SLAVE "./slave"
 #define PATH_RESULTS "./results.txt"
 
 #define NSLAVES 5
+#define MIN_TASKS 2
+#define BF_SIZE 1024
+
+#undef MAX
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 typedef struct
 {
     pid_t pid;
-    int sendInfoFD;    //devuelve resultados, slaveToMaster
-    int receiveInfoFD; //manda tarea, masterToSlave
+    int sendInfoFD;    // Devuelve resultados, slaveToMaster.
+    int receiveInfoFD; // Manda tarea, masterToSlave.
+    int ntasks;        // Tareas siendo ejecutadas por el esclavo.
 } t_slave;
-int create_slaves(t_slave slaves[]);
-int check_files(int dim, char const *files[]);
+int createSlaves(int dimSlaves, t_slave slaves[], int initialTasks, char const *files[]);
+int checkFiles(int dim, char const *files[]);
 
 int main(int argc, char const *argv[])
 {
     if (argc <= 1)
     {
         printf("Please select files.\n");
-        exit(NARGUMENTS_ERROR);
+        exit(EXIT_FAILURE);
     }
-    check_files(argc - 1, argv + 1);
-    t_slave slaves[NSLAVES];
-    create_slaves(slaves);
-
+    checkFiles(argc - 1, argv + 1);
+    int totalTasks = argc - 1;
+    int nSlaves = (totalTasks <= NSLAVES) ? totalTasks : NSLAVES;
+    int initialTasks = (totalTasks >= nSlaves * MIN_TASKS) ? MIN_TASKS : MIN_TASKS - 1;
+    t_slave slaves[nSlaves];
+    createSlaves(nSlaves, slaves, initialTasks, argv + 1);
+    int tasksDone = 0;
+    int pendingTasks = totalTasks - tasksDone;
+    fd_set fdSlaves;
+    FD_ZERO(&fdSlaves);
+    int nfds = -1;
+    while (tasksDone < totalTasks)
+    {
+        // Cargamos los fd al set para que el select puede vigilar esos fd.
+        for (int i = 0; i < nSlaves; i++)
+        {
+            int fdAux = slaves[i].sendInfoFD;
+            FD_SET(fdAux, &fdSlaves);
+            nfds = MAX(nfds, fdAux);
+        }
+        // Usamos select para vigilar los fd read del pipe slave to master.
+        int res;
+        if ((res = select(nfds + 1, &fdSlaves, NULL, NULL, NULL)) == -1)
+        {
+            perror("Error in Select");
+            exit(EXIT_FAILURE);
+        }
+        //
+        for (int i = 0; res > 0 && i < nSlaves; i++)
+        {
+            int fdCurrent = slaves[i].sendInfoFD;
+            if (FD_ISSET(fdCurrent, &fdSlaves))
+            {
+                // entonces puedo leer lo que se encuentra en el pipe.
+                // ver cuantas tareas resolvio y actualizar las tareas completadas
+                char buffer[BF_SIZE];
+                int cantRead = read(fdCurrent, buffer, BF_SIZE);
+                // Asignar si es necesario las tareas pendientes.
+                // Luego escribo la respuesta para el vista y el result.txt
+            }
+        }
+        tasksDone += res; //  ojo!! aca no es res
+        pendingTasks = totalTasks - tasksDone;
+    }
+    //Un finalizar para cerrar todos los pipes y esas cosas
     return 0;
 }
 
-int create_slaves(t_slave slaves[])
+int createSlaves(int dimSlaves, t_slave slaves[], int initialTasks, char const *files[])
 {
-    for (int i = 0; i < NSLAVES; i++)
+    int k = 0;                              // contador de tasks
+    char *slaveArguments[initialTasks + 2]; // contando NULL del final.
+    slaveArguments[0] = PATH_SLAVE;
+    slaveArguments[initialTasks + 1] = NULL;
+    for (int i = 0; i < dimSlaves; i++)
     {
         int masterToSlave[2];
         pipe(masterToSlave);
         if (pipe(masterToSlave) < 0)
         {
             perror("Pipe Error Master");
-            exit(PIPE_ERROR);
+            exit(EXIT_FAILURE);
         }
         slaves[i].receiveInfoFD = masterToSlave[WRITE];
 
@@ -65,7 +110,7 @@ int create_slaves(t_slave slaves[])
         if (pipe(slaveToMaster) < 0)
         {
             perror("Pipe Error Slave ");
-            exit(PIPE_ERROR);
+            exit(EXIT_FAILURE);
         }
         slaves[i].sendInfoFD = slaveToMaster[READ];
         int pid = fork();
@@ -74,55 +119,59 @@ int create_slaves(t_slave slaves[])
             if (close(masterToSlave[WRITE]) < 0)
             {
                 perror("Error closing slave WRITE to master");
-                exit(CLOSE_ERROR);
+                exit(EXIT_FAILURE);
             }
             if (dup2(masterToSlave[READ], READ) < 0)
             {
                 perror("Error dupping pipe");
-                exit(DUPPING_ERROR);
+                exit(EXIT_FAILURE);
             }
             // close(masterToSlave[READ]);
 
             if (close(slaveToMaster[READ]) < 0)
             {
                 perror("Error closing slave WRITE to master");
-                exit(CLOSE_ERROR);
+                exit(EXIT_FAILURE);
             }
             if (dup2(slaveToMaster[WRITE], WRITE) < 0)
             {
                 perror("Error dupping");
-                exit(DUPPING_ERROR);
+                exit(EXIT_FAILURE);
             }
-            char *args[] = {PATH_SLAVE, NULL};
-            if (execv(args[0], args) < 0)
+
+            for (int j = 1; j < initialTasks + 1; j++)
+            {
+                slaveArguments[j] = (char *)files[k++];
+                slaves[i].ntasks++;
+            }
+            if (execv(slaveArguments[0], slaveArguments) < 0)
             {
                 perror("Execv Error");
-                exit(EXEC_ERROR);
+                exit(EXIT_FAILURE);
             }
         }
         else if (pid < 0)
         {
             perror("Fork Error");
-            exit(FORK_ERROR);
+            exit(EXIT_FAILURE);
         }
         // Cierro el pipe de READ de masterToSlave
         if (close(masterToSlave[READ]) < 0)
         {
             perror("Error closing master READ to slave");
-            exit(CLOSE_ERROR);
+            exit(EXIT_FAILURE);
         }
         if (close(slaveToMaster[WRITE]) < 0)
         {
-            perror("Error closing master READ to slave");
-            exit(CLOSE_ERROR);
+            perror("Error closing slave WRITE to master");
+            exit(EXIT_FAILURE);
         }
         slaves[i].pid = pid;
-        waitpid(slaves[i].pid, NULL, 0);
     }
     return 0;
 }
 
-int check_files(int dim, char const *files[])
+int checkFiles(int dim, char const *files[])
 {
     FILE *fptr;
     for (int i = 0; i < dim; i++)
--- slave.c
+++ slave.c
@@ -1,9 +1,51 @@
+#define _POSIX_C_SOURCE 2
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SOLVER "minisat"
+#define MAX 4096
+static void runTask(char *task);
+//  | grep -o -e "Number of.*[0-9]\+" -e "CPU time.*" -e ".*SATISFIABLE"
 
 int main(int argc, char const *argv[])
 {
-    sleep(5);
-    printf("Funciona %s.\n", argv[1]);
+    // if (argc - 1 == 1)
+    //     sleep(5);
+    for (int i = 0; i < argc; i++)
+        runTask((char *)argv[i]);
+    return 0;
+}
+
+static void runTask(char *task)
+{
+    char command[MAX + 1];
+    char output[MAX + 1];
+    sprintf(command, "%s %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", SOLVER, task);
+    FILE *outputStream;
+    if ((outputStream = popen(command, "r")) == NULL)
+    {
+        perror("Erorr in popen");
+        exit(EXIT_FAILURE);
+    }
+
+    int count = fread(output, sizeof(char), MAX, outputStream);
+
+    if (ferror(outputStream))
+    {
+        perror("Error in fread");
+        exit(EXIT_FAILURE);
+    }
+
+    output[count] = 0;
+
+    printf("PID: %d\nFilename: %s\n%s\t", getpid(), task, output);
+
+    if (pclose(outputStream) == -1)
+    {
+        perror("Error in pclose");
+        exit(EXIT_FAILURE);
+    }
 }
